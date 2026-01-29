5371dddba04e64cba7d3056cbfe1316495a80ce5
Zinhle Elizabeth Hlongwane <zinhlehlongwani56@gmail.com>
2026-01-29 18:47:00 +0200

    Add initial structure for utility functions

---
+++ tests/structures.py
@@ -0,0 +1,50 @@
+
+def unique_numbers(numbers):
+    """
+    Return a set of unique numbers from a list.
+    """
+    pass
+
+
+def word_lengths(words):
+    """
+    Return a dictionary mapping each word to its length.
+    """
+    pass
+
+
+def format_report(name, score):
+    """
+    Return a formatted string:
+    "Student: <name> | Score: <score>%"
+    """
+    pass
+
+
+def recursive_sum(numbers):
+    """
+    Return the sum of a list using recursion.
+    """
+    pass
+
+
+def group_by_parity(numbers):
+    """
+    Return a dictionary grouping numbers into even and odd.
+    """
+    pass
+
+
+def countdown(n):
+    """
+    Print numbers from n down to 1 using recursion,
+    then print 'Blast off!'
+    """
+    pass
+
+
+def student_average(marks):
+    """
+    Return the average of values in a dictionary formatted to 2 decimals.
+    """
+    pass
